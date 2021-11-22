0572f0f3ac1f872129a473ef96c23af82e30758e
ZhangJian He <shoothzj@gmail.com>
2021-11-22 16:20:09 +0800

    implement pulsar bytes sink (#1)

---
+++ src/main/java/org/apache/pulsar/io/pulsar/PulsarBytesSource.java
@@ -0,0 +1,10 @@
+package org.apache.pulsar.io.pulsar;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author hezhangjian
+ */
+@Slf4j
+public class PulsarBytesSource {
+}
--- src/main/java/org/apache/pulsar/io/pulsar/PulsarSinkConfig.java
+++ src/main/java/org/apache/pulsar/io/pulsar/PulsarSinkConfig.java
@@ -1,9 +1,16 @@
 package org.apache.pulsar.io.pulsar;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.apache.pulsar.io.core.annotations.FieldDoc;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author hezhangjian
@@ -13,5 +20,57 @@ import java.io.Serializable;
 public class PulsarSinkConfig implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @FieldDoc(required = true, defaultValue = "", help = "pulsar service url")
+    private String serviceUrl;
+
+    @FieldDoc(defaultValue = "false", help = "enable pulsar client tls")
+    private boolean enableTls;
+
+    @FieldDoc(defaultValue = "false", help = "allow tls insecure connection")
+    private boolean allowTlsInsecureConnection;
+
+    @FieldDoc(defaultValue = "true", help = "")
+    private boolean enableTlsHostnameVerification;
+
+    @FieldDoc(defaultValue = "", help = "tls protocols")
+    private Set<String> tlsProtocols;
+
+    @FieldDoc(defaultValue = "", help = "tls ciphers")
+    private Set<String> tlsCiphers;
+
+    @FieldDoc(defaultValue = "", help = "use key store tls")
+    private boolean useKeyStoreTls;
+
+    @FieldDoc(defaultValue = "", help = "trust store path")
+    private String trustStorePath;
+
+    @FieldDoc(defaultValue = "", sensitive = true, help = "trust store password")
+    private String trustStorePassword;
+
+    @FieldDoc(defaultValue = "", help = "tls authentication class name")
+    private String authenticationPluginClassName;
+
+    @FieldDoc(defaultValue = "", sensitive = true, help = "authentication params")
+    private Map<String, String> authenticationParams;
+
+    @FieldDoc(required = true, defaultValue = "", help = "topic")
+    private String topic;
+
+    @FieldDoc(required = true, defaultValue = "1000", help = "max send queue size")
+    private int maxPendingMessages;
+
+    @FieldDoc(required = true, defaultValue = "false", help = "")
+    private boolean autoUpdatePartition;
+
+    public static PulsarSinkConfig load(String yamlFile) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(new File(yamlFile), PulsarSinkConfig.class);
+    }
+
+    public static PulsarSinkConfig load(Map<String, Object> map) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(new ObjectMapper().writeValueAsString(map), PulsarSinkConfig.class);
+    }
 
 }
